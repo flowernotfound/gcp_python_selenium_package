88c71f92784b1725263d600566e08299a2834de9
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-04-06 01:31:18 +0900

    add generate_date_ranges.py

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 from utils.setup_driver import setup_driver
 from utils.get_value_from_sheets import get_value_from_sheets
 from automation.automation_part import automation_part
+from utils.generate_date_ranges import generate_date_ranges
 import os
 
 URL1 = os.environ.get("URL1")
 URL2 = os.environ.get("URL2")
 COMPANY_CODE = os.environ.get("COMPANY_CODE")
-DATE_RANGES = [
-    ['2024/04/01', '2024/04/20'],
-    ['2024/05/01', '2024/05/20']
-]
-FILE_NAMES = [
-    'test01_20240401_20240420.CSV',
-    'test01_20240501_20240520.CSV'
+FILE_NAMES = [ # automate this part later
+    'facilname_01.CSV',
+    'facilname_02.CSV',
+    'facilname_03.CSV',
+    'facilname_04.CSV',
+    'facilname_05.CSV',
+    'facilname_06.CSV',
+    'facilname_07.CSV',
+    'facilname_08.CSV',
+    'facilname_09.CSV',
+    'facilname_10.CSV',
+    'facilname_11.CSV',
+    'facilname_12.CSV',
+    'facilname_13.CSV',
+    'facilname_14.CSV',
+    'facilname_15.CSV',
+    'facilname_16.CSV',
+    'facilname_17.CSV',
+    'facilname_18.CSV',
+    'facilname_19.CSV',
+    'facilname_20.CSV',
+    'facilname_21.CSV',
+    'facilname_22.CSV',
+    'facilname_23.CSV',
+    'facilname_24.CSV'
 ]
 
 def main(event, context):
+    date_ranges = [];
+    date_ranges = generate_date_ranges()
     value1, value2 = get_value_from_sheets()
     driver = setup_driver()
-    automation_part(driver, value1, value2, URL1, URL2, COMPANY_CODE, DATE_RANGES, FILE_NAMES)
+    automation_part(driver, value1, value2, URL1, URL2, COMPANY_CODE, date_ranges, FILE_NAMES)
+    print("Finished")
     driver.close()
     driver.quit()
---
+++ utils/generate_date_ranges.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+def generate_date_ranges():
+    date_ranges = []
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+
+    for month_delta in range(12):
+        month_start_date = datetime(current_year, current_month, 1) + relativedelta(months=month_delta)
+        mid_month_date = month_start_date.replace(day=15)
+        end_of_month_date = month_start_date + relativedelta(months=1) - timedelta(days=1)
+        
+        first_period_start = month_start_date.strftime('%Y/%m/%d')
+        first_period_end = mid_month_date.strftime('%Y/%m/%d')
+        second_period_start = (mid_month_date + timedelta(days=1)).strftime('%Y/%m/%d')
+        second_period_end = end_of_month_date.strftime('%Y/%m/%d')
+        
+        date_ranges.append([first_period_start, first_period_end])
+        date_ranges.append([second_period_start, second_period_end])
+    
+    return date_ranges
