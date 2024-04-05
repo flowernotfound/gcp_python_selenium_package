869c8e0cce8538b37f497d3033f589aad9f64f8a
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-04-06 02:00:35 +0900

    add delete upload_file_to_drive.py

--- utils/upload_file_to_drive.py
+++ utils/upload_file_to_drive.py
@@ -5,13 +5,23 @@ import os
 
 TARGET_FOLDER = os.environ.get("TARGET_FOLDER_ID")
 
-def upload_file_to_drive(file_name): # upload file to Google Drive
+def delete_existing_file(service, file_name, folder_id):
+    query = f"name = '{file_name}' and '{folder_id}' in parents and trashed = false"
+    response = service.files().list(q=query, spaces='drive', fields='files(id, name)').execute()
+    for file in response.get('files', []):
+        print(f"Deleting file: {file.get('name')} (ID: {file.get('id')})")
+        service.files().delete(fileId=file.get('id')).execute()
+
+def upload_file_to_drive(file_name):
     creds = get_credentials()
     service = build('drive', 'v3', credentials=creds)
+    delete_existing_file(service, file_name, TARGET_FOLDER)
+
     file_metadata = {
         'name': file_name,
         'parents': [TARGET_FOLDER]
     }
+    
     media = MediaFileUpload(f'/tmp/{file_name}', mimetype='text/csv')
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     print(f'Uploaded file ID: {file.get("id")}')
