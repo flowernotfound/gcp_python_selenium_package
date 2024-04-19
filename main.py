b5b997b19de2827336bc068b64da5dcdce56c31e
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-04-20 01:49:25 +0900

    fixed generate_date_range.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from utils.setup_driver import setup_driver
 from utils.get_value_from_sheets import get_value_from_sheets
 from automation.reservation import reservation_automation
 from automation.analyze import analyze_automation
-from utils.generate_date_ranges import generate_date_ranges
+from utils.generate_date_ranges import generate_date_half_ranges
+from utils.generate_date_ranges import generate_monthly_date_ranges
 import os
 
 URL1 = os.environ.get("URL1")
@@ -13,7 +14,7 @@ FACILITY_NAME = os.environ.get("FACILITY_NAME")
 def reservation_entry(event, context):
     print("Start : reservation")
     date_ranges = [];
-    date_ranges = generate_date_ranges()
+    date_ranges = generate_date_half_ranges()
     date_len = len(date_ranges)
     file_names = [f"{FACILITY_NAME}_{i:02d}.CSV" for i in range(1, date_len + 1)]
     value1, value2 = get_value_from_sheets()
@@ -26,7 +27,7 @@ def reservation_entry(event, context):
 def analyze_entry(event, context):
     print("Start : analyze")
     date_ranges = [];
-    date_ranges = generate_date_ranges()
+    date_ranges = generate_monthly_date_ranges()
     date_len = len(date_ranges)
     file_names = [f"{FACILITY_NAME}_analyze_{i:02d}.CSV" for i in range(1, date_len + 1)]
     value1, value2 = get_value_from_sheets()
--- utils/generate_date_ranges.py
+++ utils/generate_date_ranges.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
-def generate_date_ranges():
+def generate_date_half_ranges():
     date_ranges = []
     current_year = datetime.now().year
     current_month = datetime.now().month
@@ -18,5 +18,21 @@ def generate_date_ranges():
         
         date_ranges.append([first_period_start, first_period_end])
         date_ranges.append([second_period_start, second_period_end])
+    
+    return date_ranges
+
+def generate_monthly_date_ranges():
+    date_ranges = []
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+
+    for month_delta in range(12):
+        month_start_date = datetime(current_year, current_month, 1) + relativedelta(months=month_delta)
+        end_of_month_date = month_start_date + relativedelta(months=1) - timedelta(days=1)
+        
+        period_start = month_start_date.strftime('%Y/%m/%d')
+        period_end = end_of_month_date.strftime('%Y/%m/%d')
+        
+        date_ranges.append([period_start, period_end])
     
     return date_ranges
