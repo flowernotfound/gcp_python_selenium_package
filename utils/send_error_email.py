a848906a776207d0bc1c8cfd32f59ebaad114a4a
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-05-14 09:56:37 +0900

    add env send_error_email.py

--- utils/send_error_email.py
+++ utils/send_error_email.py
@@ -2,14 +2,16 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
+SENDER_MAIL = os.environ.get("SENDER_MAIL")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+RECIPIENT_MAIL = os.environ.get("RECIPIENT_MAIL")
 
 def send_error_email(error_message):
     smtp_server = 'smtp.gmail.com'
     smtp_port = 587
-    sender_email = 'thnksfrth3244@gmail.com'
+    sender_email = SENDER_MAIL
     sender_password = MAIL_PASSWORD
-    recipient_email = 'thnksfrth3244@gmail.com'
+    recipient_email = RECIPIENT_MAIL
 
     subject = 'エラー通知: ねっぱんデータ取得'
     body = f"ねっぱんのスクリプトの実行中にエラーが発生しました。\n\nエラー内容:\n{error_message}"
